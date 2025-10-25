355caed5914b177dfd8420ee450880bfb45da7fa
Chan2244 <booth.chandler@yahoo.com>
2025-10-24 19:01:15 -0600

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiUrl = "https://iskarr.github.io/austindonovan.gethub.io/api/business.json"
+
+        fetch(https://iskarr.github.io/austindonovan.gethub.io/api/business.json)
+        .then((response) => response.json())
+        .then((jsObject) => {
+            console.log(jsObject);
+        });
